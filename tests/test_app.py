7fcc45be23f36aa22b93cba49a6754ae08ecaae5
sai prakash <saiprakash53287@gmail.com>
2025-11-03 14:12:24 +0530

    Added automated testing to CI/CD pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+from app.app import app
+
+def test_home_route():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello from Secure Delivery Automation!" in response.data
